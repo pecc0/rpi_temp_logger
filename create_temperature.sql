4e31af627d06e6ba15d347ae6ce6a176c6c64225
petko <air_biker@abv.bg>
2017-05-24 22:35:03 +0300

    fixed timestamp

--- create_temperature.sql
+++ create_temperature.sql
@@ -4,7 +4,7 @@ USE temperatures;
 
 DROP TABLE IF EXISTS temps;
 CREATE TABLE temps (
-	measure_time DATETIME NOT NULL DEFAULT NOW(),
+	measure_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	temperature FLOAT,
 	humidity FLOAT
 )
